1fc5b2d112cbda64ef158889503171549eca1d6a
Mathenge <anthonykihagi@gmail.com>
2020-04-13 21:48:58 +0300

    Update postgres

--- swapi/settings.py
+++ swapi/settings.py
@@ -50,7 +50,7 @@ WSGI_APPLICATION = 'swapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sql'),
     }
 }
